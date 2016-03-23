2f86baa170768f1234065422db1e457f683b5c7e
vagrant <vagrant@precise64.(none)>
2016-03-23 06:07:49 +0000

    deploying to ElasticBeanstalk

--- application.py
+++ application.py
@@ -20,6 +20,33 @@ import settings
 
 # EB looks for an 'application' callable by default.
 application = Flask(__name__)
+session = makeSession()
+
+#.............................................................................................
+#.....Helper functions.............................................................................
+#.............................................................................................
+
+# Takes in a dictionary of results (question1=a, question2=b, etc)
+# Returns a dictionary of scores for each personality aspect (ex. N = 18)
+def scoreResults(results):
+	print "...In scoreResults............"
+	scores = {'e':0, 'i':0, 's':0, 'n':0, 't':0, 'f':0, 'j':0, 'p':0}
+	personalities = ['z','e','i','s','n','s','n','t','f','t','f','j','p','j','p']
+	print "...before scoring: %s ................." % scores
+	for x in range(0,9): #<-----------------------this must be corrected to 0,10 once the db is corrected to have 70 questions
+		for i,a in enumerate(range(1,14,2)):
+			b = a+1
+			#print i+1+x*7,a,b
+			if results[i+1+x*7] == 'a':
+				scores[personalities[a]] += 1
+			elif results[i+1+x*7] == 'b':
+				scores[personalities[b]] += 1
+			else:
+				print "...ERROR in scoreResutls(): Invalid input on question %s ............." % str(n+7*x)
+	print "...after scoring: %s ..............." % scores
+	
+	return scores
+
 
 #.............................................................................................
 #.....GET Requests.............................................................................
@@ -51,14 +78,19 @@ def testEngine():
 	print ".............................................."
 	return render_template('questions.html')
 
-
 # Show all Questions
 @application.route('/questions')
 def showQuestions():
-	session = makeSession()
-	print "...makeSession() successful......"
 	questions = session.query(Questions).order_by(Questions.number)
 	return render_template('questions.html', questions=questions)
+
+# Pull all stored results from database and display them
+@application.route('/allResults')
+def showAllScores():
+	scores = session.query(Results).all()
+	for score in scores:
+		print score
+	return render_template('allScores.html', scores=scores)
 	
 #.............................................................................................
 #.....GET Requests.............................................................................
@@ -67,15 +99,35 @@ def showQuestions():
 # Show results
 @application.route('/results', methods=['GET','POST'])
 def showResults():
+	print "...In showResults().................."
 	if request.method == 'POST':
-		newResults = Results(name = request.form['name'],
-		user_id=login_session['user_id'])
+		#print "...in /results if POST......"
+		results = {}
+		for n in range(1,69):
+			result = "result" + str(n)	
+			#print result
+			results.update({n:str(request.form[result])})
+		print results
+		# send dictionary results, returns dictionary scores
+		scores = scoreResults(results)	
+		# ToDo: Include user in commit in order to store results by user using Oauth2
+		newResults = Results(I = scores['i'],
+							E = scores['e'],
+							N = scores['n'],
+							S = scores['s'],
+							T = scores['t'],
+							F = scores['f'],
+							J = scores['j'],
+							P = scores['p'],)
+		#user_id=login_session['user_id'])
 		session.add(newResults)
+		#flash('New Menu %s Item Successfully Created' % (newItem.name))
 		flash('New Results Successfully Saved')
 		session.commit()
-		return render_template('questions.html')	  
+		print "...session sucessfully committed.........."
+		return render_template('results.html', scores = scores)
 	else:
-		return redirect(url_for('results'))
+		return render_template('questions.html')
 	
 #.............................................................................................
 #.....Boiler plate.............................................................................
@@ -85,6 +137,7 @@ def showResults():
 if __name__ == "__main__":
     # Setting debug to True enables debug output. This line should be
     # removed before deploying a production app.
-    #application.debug = True
-    application.run()
-    #application.run(host = '0.0.0.0', port = 5000)
+	application.secret_key = 'super_secret_key'
+	#application.debug = True
+	application.run()
+	#application.run(host = '0.0.0.0', port = 5000)
